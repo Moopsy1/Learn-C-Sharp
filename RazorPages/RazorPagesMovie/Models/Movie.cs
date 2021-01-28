33cce979a3ead919b92a865e952f62443a30d8c2
Moopsy1 <anton-munnik@hotmail.com>
2021-01-28 20:54:44 +0200

    Updated razor pages with CRUD

--- RazorPages/RazorPagesMovie/Models/Movie.cs
+++ RazorPages/RazorPagesMovie/Models/Movie.cs
@@ -6,13 +6,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesMovie.Models
 {
+
     public class Movie
     {
+        /* classes are added for managing movies in a database.
+          The apps model classes use Entity Framework Core (EF Core) to work with the database. 
+         EF Core is an object-relational mapper (O/RM) that simplifies data access. 
+         You write the model classes first, and EF Core creates the database.
+        */
+
         //field named ID becomes the private key
         public int ID { get; set; }
         public string Title { get; set; }
 
         //using System.ComponentModel.DataAnnotations;
+        /* [DataType(DataType.Date)]: The[DataType] attribute specifies the type of the data(Date). With this attribute:
+        The user isn't required to enter time information in the date field.
+        Only the date is displayed, not time information.*/
+
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
---
+++ RazorPages/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+/*        When a request is made for the page,
+ the OnGetAsync method returns a list of movies to the Razor Page.
+On a Razor Page, OnGetAsync or OnGet is 
+called to initialize the state of the page.*/
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
